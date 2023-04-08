d6f506e3e9873438ce495298339ef947e823850b
cambei <54690836+cambeius@users.noreply.github.com>
2023-04-08 10:13:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2-400, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("blue")
+
+    ##pygame.draw.circle(screen, "red", player_pos, 40)
+    avatar = pygame.image.load("eggzu.png")
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+        ##avatar.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    avatarRender()
+
+    pygame.display.flip()
+
+    # limits FPS to 60
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
